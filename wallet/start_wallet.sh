c9a34836178e314d4c6ef2a80793d4b60c88084b
root <root@localhost.localdomain>
2019-07-08 20:59:08 +0900

    add an option of --data-dir at wallet/start_wallet.sh

--- wallet/start_wallet.sh
+++ wallet/start_wallet.sh
@@ -8,4 +8,5 @@ WALLETHOST="127.0.0.1"
 WALLETPORT="8899"
 
 ./stop_wallet.sh
-$EOSBINDIR/keosd/keosd --config-dir $WALLETDIR --wallet-dir $WALLETDIR --http-server-address $WALLETHOST:$WALLETPORT $@ & echo $! > $WALLETDIR/wallet.pid
+# $EOSBINDIR/keosd/keosd --config-dir $WALLETDIR --wallet-dir $WALLETDIR --http-server-address $WALLETHOST:$WALLETPORT $@ & echo $! > $WALLETDIR/wallet.pid
+$EOSBINDIR/keosd/keosd --config-dir $WALLETDIR --data-dir $WALLETDIR --http-server-address $WALLETHOST:$WALLETPORT $@ & echo $! > $WALLETDIR/wallet.pid
